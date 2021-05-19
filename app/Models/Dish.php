488fd8f6e70dca68e07f873664c09f3cf62892ad
Phu1237 <phu1237@gmail.com>
2021-05-19 17:18:58 +0700

    model: update Dish model
    add invoices function to get all invoices have this dish

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Dish extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+    ];
+
+    /**
+     * Get all invoices have this dish
+     */
+    public function invoices()
+    {
+        return $this->hasManyThrough(Invoice::class, InvoiceDishes::class);
+    }
 }
